4c37d455a87b1d71b458249115aea7f805629003
EkicalPog <ekically@gmail.com>
2021-10-14 22:49:16 -0200

    plz no

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -58,9 +58,9 @@ class TitleState extends MusicBeatState
 
 	override public function create():Void
 	{
-		#if polymod
+		/*#if polymod
 		polymod.Polymod.init({modRoot: "mods", dirs: ['introMod']});
-		#end
+		#end*/
 		
 		#if sys
 		if (!sys.FileSystem.exists(Sys.getCwd() + "/assets/replays"))
