8443d6d27487597d2304d9b6144db9c7a7edd38e
ChromaSen <ew3cad@rambler.ru>
2022-02-05 03:51:48 +0300

    midsong events yes

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5196,20 +5196,22 @@ class PlayState extends MusicBeatState
 			iconP1.alpha = (health-0.25)/2+0.2;
 			switch (curStep)
 			{
-				case 128:
-					 defaultCamZoom = 0.9;
-				case 253:
-					 defaultCamZoom = 1.2;
-				case 409:
-					 defaultCamZoom = 1.1;
-				case 413: 
-					defaultCamZoom = 0.95;		
-				case 516: 
-					 defaultCamZoom = 0.85;
-				case 768: 
-					 defaultCamZoom = 1.1;
-				case 1039:
-					 defaultCamZoom = 0.85;
+				case 128: defaultCamZoom = 0.9;
+				case 253: defaultCamZoom = 1.2;
+				case 409: defaultCamZoom = 1.1;
+				case 413: defaultCamZoom = 0.95;	
+				case 513: defaultCamZoom = 0.85;
+				case 518: defaultCamZoom = 0.9;
+				case 528: defaultCamZoom = 0.95;
+				case 535: defaultCamZoom = 1;
+				case 540: defaultCamZoom = 0.9;
+				case 575: defaultCamZoom = 1.1;
+				case 582: defaultCamZoom = 1.05;
+				case 592: defaultCamZoom = 0.98;
+				case 599: defaultCamZoom = 1.15;
+				case 639: defaultCamZoom = 0.85;
+				case 768: defaultCamZoom = 1.1;
+				case 1039: defaultCamZoom = 0.85; // shit ton of code because yeah
 			}
 					if ((curStep >= 254) && (curStep <= 518))
 						{
